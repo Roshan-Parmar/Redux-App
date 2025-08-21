ae07f8184b50845391963a347dfcf6badfa362fb
Roshan Parmar <roshansinghparmar77@gmail.com>
2025-08-21 19:30:44 +0530

    Change in routes

---
+++ src/routes/MainRoutes.jsx
@@ -0,0 +1,16 @@
+import Home from "../pages/Home"
+
+const MainRoutes = () => {
+  return (
+    <>
+     <Routes>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/home" element={<Home/>}/>
+     </Routes> 
+    </>
+  )
+}
+
+export default MainRoutes
